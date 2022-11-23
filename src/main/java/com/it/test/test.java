d67695a6e7f2b5ba0d831e4b9f51367078feef35
DW <2387951829@qq.com>
2022-11-23 15:21:33 +0800

    first commit

---
+++ src/main/java/com/it/test/test.java
@@ -0,0 +1,10 @@
+package com.it.test;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
