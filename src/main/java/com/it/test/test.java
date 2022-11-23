b7f4a5979b3c0ace8b63600cb2ff00cec79bca1b
DW <2387951829@qq.com>
2022-11-23 15:26:35 +0800

    second commit

--- src/main/java/com/it/test/test.java
+++ src/main/java/com/it/test/test.java
@@ -5,6 +5,7 @@ import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
